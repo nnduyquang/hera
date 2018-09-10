7917da3d68460bbbf9c093edab96647df8d92a88
loiPH <loiph.vncoder@users.noreply.github.com>
2018-09-10 16:16:40 +0700

    update

--- local/resources/views/frontend/home/index.blade.php
+++ local/resources/views/frontend/home/index.blade.php
@@ -23,10 +23,10 @@
 @section('slider')
     @include('frontend.common.slider')
     @include('frontend.home.popup')
+    @include('frontend.home.nav')
 @stop
 
 @section('container')
-
 
     @include('frontend.home.h_1')
     @include('frontend.home.h_2')
@@ -37,4 +37,98 @@
 @stop
 @section('jv-scripts')
     {{ Html::script('js/fe_h_index.js') }}
+    <script>
+
+
+
+        window.onscroll = function() {myFunction()};
+
+        var navbar = document.getElementById("h_1");
+        var navbar2 = document.getElementById("h_2");
+        var navbar3 = document.getElementById("h_3");
+        var navbar4 = document.getElementById("h_4");
+        var sticky = navbar.offsetTop-130;
+        var sticky2 = navbar2.offsetTop-160;
+        var sticky3 = navbar3.offsetTop-160;
+        var sticky4 = navbar4.offsetTop-168;
+
+        function myFunction() {
+            var i;
+            if (window.pageYOffset >= sticky4) {
+                // alert (123);
+
+                tablinks = document.getElementsByClassName("nav-r");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+                $("#h_4_nav").addClass("active");
+                // navbar.classList.add(" active")
+            }else if(window.pageYOffset >= sticky3){
+
+                tablinks = document.getElementsByClassName("nav-r");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+                $("#h_3_nav").addClass("active");
+            }
+            else if(window.pageYOffset >= sticky2){
+
+                tablinks = document.getElementsByClassName("nav-r");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+                $("#h_2_nav").addClass("active");
+            }else if(window.pageYOffset >= sticky){
+
+                tablinks = document.getElementsByClassName("nav-r");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+                $("#h_1_nav").addClass("active");
+            }
+
+            else {
+                tablinks = document.getElementsByClassName("nav-r");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+                $("#h_top_nav").addClass("active");
+            }
+        }
+
+        $("#h_top_nav").click(function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top-138
+            }, 1000);
+        });
+
+        $("#h_4_nav").click(function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $("#h_4").offset().top-138
+            }, 1000);
+        });
+
+        $("#h_3_nav").click(function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $("#h_3").offset().top-110
+            }, 1000);
+        });
+
+        $("#h_2_nav").click(function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $("#h_2").offset().top-110
+            }, 1000);
+        });
+
+        $("#h_1_nav").click(function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $("#h_1").offset().top-110
+            }, 1000);
+        });
+    </script>
 @stop
---
+++ local/resources/views/frontend/home/nav.blade.php
@@ -0,0 +1,39 @@
+<style>
+    #nav{
+
+        position: fixed;
+        top: 50%;
+        right: 20px;
+        transform: translateY(-50%);
+        z-index: 20;
+
+    }
+
+    #nav ul{
+        list-style-type: none;
+    }
+
+    #nav ul li{
+        padding-bottom: 6px;
+    }
+
+    #nav ul li a{
+        color: white;
+        font-size: 12px;
+    }
+
+    #nav ul li a.active{
+        color: #007bff;
+    }
+</style>
+
+<div id="nav">
+    <ul>
+        <li><a class="nav-r" id="h_top_nav" href=".slider"><i class="fab fa-gg-circle"></i></a></li>
+        <li><a class="nav-r" id="h_1_nav" href="#h_1"><i class="fab fa-gg-circle"></i></a></li>
+        <li><a class="nav-r" id="h_2_nav" href="#h_2"><i class="fab fa-gg-circle"></i></a></li>
+        <li><a class="nav-r" id="h_3_nav" href="#h_3"><i class="fab fa-gg-circle"></i></a></li>
+        <li><a class="nav-r" id="h_4_nav" href="#h_4"><i class="fab fa-gg-circle"></i></a></li>
+    </ul>
+
+</div>
