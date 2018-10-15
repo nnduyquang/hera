f64a730f3ceae98bc5afc9a3889a1ea5991702af
nnduyquang@gmail.com <bunvodich>
2018-10-15 13:46:44 +0700

    ok

--- local/resources/views/frontend/common/menu/hera-mobile-menu.blade.php
+++ local/resources/views/frontend/common/menu/hera-mobile-menu.blade.php
@@ -14,26 +14,31 @@
         <ul>
 
             @foreach($listMenu as $key=>$item)
-                <li class="li-overlay"><a class="{{ request()->is($item->link()) ? 'active' : '/' }}"
-                                          href="{{URL::to($item->link())}}">{{$item->title}}</a>
-                    @if(!$item->children->isEmpty())
-                        @include('frontend.common.menu.list-menu-mobile-item', ['subMenu' => $item->children])
-                    @endif
-                </li>
-            @endforeach
-            <li class="text-center pt-4"><a
-                        href="{{URL::asset('/dat-tiec/')}}" class="">ĐẶT TIỆC NGAY</a></li>
-            <li class="text-center">
-                <a href=""><i class="fab fa-facebook-f"></i></a>
-                <a href=""><i class="fab fa-instagram"
-                              style="background-image:url({{URL::asset('images/bg/bg-insta.jpg')}});"></i></a>
-                <a href=""><i class="fab fa-youtube"></i></a>
-            </li>
-            <li class="text-center">
+                @if($item->children->isEmpty())
+                    <li class="li-overlay"><a class="{{ request()->is($item->link()) ? 'active' : '/' }}"
+                                              href="{{URL::to($item->link())}}">{{$item->title}}</a>
+                @else
+                    <li class="li-overlay"><a class="{{ request()->is($item->link()) ? 'active' : '/' }}"
+                                              href="javascript:void(0)">{{$item->title}}</a>
+                        @endif
+                        @if(!$item->children->isEmpty())
+                            @include('frontend.common.menu.list-menu-mobile-item', ['subMenu' => $item->children])
+                        @endif
+                    </li>
+                    @endforeach
+                    <li class="text-center pt-4"><a
+                                href="{{URL::asset('/dat-tiec/')}}" class="">ĐẶT TIỆC NGAY</a></li>
+                    <li class="text-center">
+                        <a href=""><i class="fab fa-facebook-f"></i></a>
+                        <a href=""><i class="fab fa-instagram"
+                                      style="background-image:url({{URL::asset('images/bg/bg-insta.jpg')}});"></i></a>
+                        <a href=""><i class="fab fa-youtube"></i></a>
+                    </li>
+                    <li class="text-center">
 
 
-                <p class="pb-3"><span><i class="fas fa-phone-volume"></i></span>
-                    {{$listFrontEndInfo['hotline']}} (Hotline)</p></li>
+                        <p class="pb-3"><span><i class="fas fa-phone-volume"></i></span>
+                            {{$listFrontEndInfo['hotline']}} (Hotline)</p></li>
 
         </ul>
     </div>
